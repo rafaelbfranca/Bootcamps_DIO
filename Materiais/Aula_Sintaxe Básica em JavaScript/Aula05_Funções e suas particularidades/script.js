3dfb2b2c2a407cf1b6603360e72251bb7b43d76f
Rafael França <98033346+rafaelbfranca@users.noreply.github.com>
2022-04-22 20:59:40 -0300

    Aula 05

---
+++ Materiais/Aula_Sintaxe Básica em JavaScript/Aula05_Funções e suas particularidades/script.js
@@ -0,0 +1,11 @@
+function funcao() {
+    console.log("Essa mensagem veio de uma função sem parâmetros.");
+}
+
+funcao();
+
+function func(primeiro, segundo) {
+    console.log(primeiro, segundo);
+}
+
+func("Essa mensagem veio de","uma função com parâmetros.");
