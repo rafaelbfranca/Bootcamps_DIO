55471bf9de939dcf1654dee39a395ea0b05034fd
Rafael França <98033346+rafaelbfranca@users.noreply.github.com>
2022-04-25 20:39:59 -0300

    Atividade Prática

---
+++ Materiais/Aula_Trabalhando com Módulos em JavaScript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("rafael",47));
+console.log(mostraCidade("Rafael", "Curitiba"));
+console.log(mostraHobby("Rafael", "ouvir música"));
