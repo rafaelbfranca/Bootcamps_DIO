36699bf3f4498209b76a716f3cf4e1a74e97f3bc
Rafael França <98033346+rafaelbfranca@users.noreply.github.com>
2022-05-03 12:47:29 -0300

    Desafio 2 de 3 - Fases da Lua

---
+++ Materiais/Desafios de Código Iniciais Js - Philips Developer/2 de 3 - Fases da Lua/script.js
@@ -0,0 +1,19 @@
+//Abaixo o código entregue no desafio.
+
+/*
+let lines = gets().split('\n');
+
+let line = lines.shift().split(" ");
+let inicio = parseInt(line[0]);
+let final = parseInt(line[1]);
+
+if (inicio >= 0 && final <= 2) {
+    print('nova\n');
+} else if (final > inicio && final <= 96 && final >= 3) {
+    print('crescente\n');
+} else if (final < inicio && final <= 96 && final >= 3) {
+    print('minguante\n');
+} else { 
+    print('cheia\n');
+}
+*/
