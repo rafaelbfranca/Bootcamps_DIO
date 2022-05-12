8aacf31da364a0db8fdf66a3ae79feae577e9afe
Rafael França <98033346+rafaelbfranca@users.noreply.github.com>
2022-05-11 22:46:50 -0300

    Aulas
    Update

--- Materiais/Aula_Explorando Diretivas com Angular/ambiente-angular/src/app/app.component.ts
+++ Materiais/Aula_Explorando Diretivas com Angular/ambiente-angular/src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  count = 0;
+  nome = "Francisco Ferreira Santos";
+  text = "";
+
+  pessoas = [
+    {
+      nome: "Antônio",
+      sobrenome: "Pereira"
+    },
+    {
+      nome: "Paulo",
+      sobrenome: "Cardoso"
+    },
+    {
+      nome: "Mônica",
+      sobrenome: "Carvalho"
+    },
+    {
+      nome: "Tânia",
+      sobrenome: "Aparecida"
+    }
+  ]
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    let interval = setInterval(() => {
+      this.count++;
+      if (this.count === 10) {
+        clearInterval(interval)
+      }
+    }, 1000)
+  }
+
+  clicou(nome: string): void{
+    console.log(nome, 'clicou em mim.')
+  }
 
 }
