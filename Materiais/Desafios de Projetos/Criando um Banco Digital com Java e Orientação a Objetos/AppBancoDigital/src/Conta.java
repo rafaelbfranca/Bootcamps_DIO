e95bfda2ff137b78fcda2c939ac6add150a2caf7
Rafael França <98033346+rafaelbfranca@users.noreply.github.com>
2022-06-02 16:52:08 -0300

    Desafio Banco Digital

---
+++ Materiais/Desafios de Projetos/Criando um Banco Digital com Java e Orientação a Objetos/AppBancoDigital/src/Conta.java
@@ -0,0 +1,63 @@
+//Classe definida como abstrata apenas para evitar que seja instanciada sem ser através de ContaCorrente ou ContaPoupanca.
+public abstract class Conta implements IConta{
+	
+	private static final int AGENCIA_PADRAO = 1;
+	private static int SEQUENCIAL = 1;
+	
+	protected int agencia;
+	protected int numero;
+	protected String tipo;
+	protected double saldo;
+	protected Cliente cliente;
+	
+	public Conta(Cliente cliente){
+		this.agencia = Conta.AGENCIA_PADRAO;
+		this.numero = SEQUENCIAL++;
+		this.cliente = cliente;
+	}
+	
+	@Override
+	public void sacar(double valor) {
+		this.saldo -= valor;
+	}
+
+	@Override
+	public void depositar(double valor) {
+		this.saldo += valor;		
+	}
+
+	@Override
+	public void transferir(double valor, Conta contaDestino) {
+		this.sacar(valor);
+		contaDestino.depositar(valor);
+	}
+	
+	@Override
+	public void imprimirExtrato(String texto) {
+		System.out.println(texto);
+		System.out.println("Cliente: " + this.cliente.getName());
+		System.out.println(String.format("Agencia: %d", this.agencia));
+		System.out.println(String.format("Numero: %d", this.numero));
+		System.out.println(String.format("Saldo: %.2f", this.saldo));
+	}
+	
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+
+	protected void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+}
