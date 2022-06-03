f9d53952b44b8c3c8c25085da7e56305e8fb6582
Rafael França <98033346+rafaelbfranca@users.noreply.github.com>
2022-06-03 10:13:25 -0300

    Update Main.java

--- Materiais/Desafios de Projetos/Abstraindo um Bootcamp Usando Orientação a Objetos em Java/desafio-dio-POO/src/Main.java
+++ Materiais/Desafios de Projetos/Abstraindo um Bootcamp Usando Orientação a Objetos em Java/desafio-dio-POO/src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -15,13 +17,40 @@ public class Main {
         curso2.setDescricao("Curso de Criacao de Sites com HTML5");
         curso2.setCargaHoraria(8);
 
-
         Mentoria mentoria1 = new Mentoria();
         mentoria1.setTitulo("Java");
         mentoria1.setDescricao("Mentoria de Programacao Java");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(mentoria1);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Cursos e Mentorias de Programação Web.");
+        bootcamp.getConteudo().add(curso1);
+        bootcamp.getConteudo().add(curso2);
+        bootcamp.getConteudo().add(mentoria1);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Pedro");
+        dev1.inscreverBootcamp(bootcamp);
+        dev1.progredir();
+        dev1.progredir();
+        
+        Dev dev2 = new Dev();
+        dev2.setNome("Paula");
+        dev2.inscreverBootcamp(bootcamp);
+        dev2.progredir();
+
+
+        // System.out.println("Conteudos Inscritos de "+dev1.getNome()+": "+dev1.getConteudosInscritos());
+        // System.out.println();
+        // System.out.println("Conteudos Concluídos de "+dev1.getNome()+": "+dev1.getConteudosConcluidos());
+        // System.out.println();
+        // System.out.println("XP: "+dev1.calcularTotalXp());
+        // System.out.println();
+        // System.out.println("Conteudos Inscritos de "+dev2.getNome()+": "+dev2.getConteudosInscritos());
+        // System.out.println();
+        // System.out.println("Conteudos Concluídos de "+dev2.getNome()+": "+dev2.getConteudosConcluidos());
+        // System.out.println("XP: "+dev2.calcularTotalXp());
+        // System.out.println();
     }
 }
