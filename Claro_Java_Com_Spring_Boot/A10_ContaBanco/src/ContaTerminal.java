cc31a6c4ee9cc98ff775ea64ede188b6180db08a
Rafael França <98033346+rafaelbfranca@users.noreply.github.com>
2024-08-06 12:22:50 -0300

    Update 24080603
    Desenvolvimento da classe ContaTerminal.

--- Claro_Java_Com_Spring_Boot/A10_ContaBanco/src/ContaTerminal.java
+++ Claro_Java_Com_Spring_Boot/A10_ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite o número da conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o número da agência:");
+        int numeroAgencia = scanner.nextInt();
+
+        System.out.println("Digite o nome do cliente:");
+        scanner.nextLine();//Comando para consumir o "\n" digitado ao final da entrada anterior. REF: https://stackoverflow.com/questions/23450524/java-scanner-doesnt-wait-for-user-input
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o saldo da conta:");
+        double saldoConta = scanner.nextDouble();
+
+        System.out.println("Conta criada com sucesso. Verifique os dados abaixo.");
+        System.out.println("Agência: "+numeroAgencia);
+        System.out.println("Conta: "+numeroConta);
+        System.out.println("Nome: "+nomeCliente);
+        System.out.println("Saldo: "+saldoConta);
+
+        scanner.close();
     }
 }
